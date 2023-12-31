8e04f8777e7dbddf6cc15ea895b54441feeb36c7
SanskarDhyani98 <sanskardhyani98@gmail.com>
2023-12-31 13:14:10 +0530

    daily

--- December30_winner.cpp
+++ December30_winner.cpp
@@ -44,6 +44,18 @@ class Solution{
         }
         
         return ans;
+        //  int i, maxi=0;
+        // vector<string> v;
+        // unordered_map<string, int> mp;
+        // for(i=0;i<n;i++) mp[a[i]]++;
+        // for(auto x: mp) {
+        //     maxi=max(maxi, x.second);
+        // }
+        // for(auto x: mp) {
+        //     if(x.second==maxi) v.push_back(x.first);
+        // }
+        // sort(v.begin(), v.end());
+        // return {v[0], to_string(maxi)};
     }
 };
 
