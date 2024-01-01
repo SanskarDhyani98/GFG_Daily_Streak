7101ffa97fad781a4d1bf25747402fef50ed5c53
SanskarDhyani98 <sanskardhyani98@gmail.com>
2024-01-01 15:31:46 +0530

    new approach

--- 1jan_pair.cpp
+++ 1jan_pair.cpp
@@ -15,27 +15,42 @@ Video Solution : https://youtu.be/ETYGE6QcI1U
 using namespace std;
 
 // } Driver Code Ends
+// class Solution {
+//   public:
+//     bool canPair(vector<int> nums, int k) {
+//         map<int,int> f;
+        
+//         for(int i = 0; i < nums.size(); i++){
+//             int cur = nums[i] % k;
+//             int need = (k - cur) % k;
+            
+//             if(f.count(need)){
+//                 --f[need];
+                
+//                 if(f[need] == 0)
+//                     f.erase(need);
+//             }
+//             else{
+//                 ++f[cur];
+//             }
+//         }
+        
+//         return f.size() == 0;
+//     }
+// };
 class Solution {
   public:
     bool canPair(vector<int> nums, int k) {
-        map<int,int> f;
-        
-        for(int i = 0; i < nums.size(); i++){
-            int cur = nums[i] % k;
-            int need = (k - cur) % k;
-            
-            if(f.count(need)){
-                --f[need];
-                
-                if(f[need] == 0)
-                    f.erase(need);
-            }
-            else{
-                ++f[cur];
-            }
-        }
-        
-        return f.size() == 0;
+      int n = nums.size();
+        if(n % 2) return false;
+        vector<int> mp(k + 1, 0); 
+        for(auto x : nums){
+            x %= k;
+            mp[x]++;
+            mp[k-x]--;
+        } 
+        for(int i = 1; i < k; i++) if(mp[i]>0) return false;
+        return true;
     }
 };
 
